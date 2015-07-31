f9c0ac2be0702ede8e47698a53a60ece7c2fc6ef
wenjie.li <liwenjie0912@126.com>
2015-07-31 22:51:17 +0800

    add mjrefresh

--- Pods/Target Support Files/Pods-VerLabel/Pods-VerLabel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VerLabel : NSObject
-@end
-@implementation PodsDummy_Pods_VerLabel
-@end
---
+++ Pods/Target Support Files/VerLabel/VerLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VerLabel : NSObject
+@end
+@implementation PodsDummy_VerLabel
+@end
